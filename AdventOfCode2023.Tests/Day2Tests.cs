f117b6815a8dbffde76b54cf1df1d00c6cf54347
Pommier Valentin <pommier.valentin-ext@solvay.com>
2023-12-03 17:46:56 +0100

    feat: advent of code day 2 setup

---
+++ AdventOfCode2023.Tests/Day2Tests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023.Tests
+{
+    public class Day2Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Should_parse_sets_from_a_game_with_nombers_of_green_blue_and_red_cubes_used_in_each_set()
+        {
+
+        }
+    }
+}
