78ebfeba80e16cd8400e4d61c68821f5c20c2002
Pommier Valentin <pommier.valentin-ext@solvay.com>
2023-12-04 11:52:53 +0100

    feat: day 4 parse card

---
+++ AdventOfCode2023.Tests/Day4Tests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCode2023Day4;
+
+namespace AdventOfCode2023.Tests
+{
+    public class Day4Tests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void Should_return_a_card_containing_winning_numbers_and_numbers_to_play_from_string_input()
+        {
+            var input = "Card 1: 41 | 83";
+
+            var card = new Card(input);
+
+            Assert.That(card.WinningNumbers.First(), Is.EqualTo(41));
+            Assert.That(card.Numbers.First(), Is.EqualTo(83));
+            Assert.That(card.Id, Is.EqualTo(1));
+        }
+    }
+}
---
+++ AdventOfCode2023Day4/Card.cs
@@ -0,0 +1,42 @@
+﻿using Kernel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023Day4
+{
+    public class Card
+    {
+        public int Id { get; private set; }
+        public List<int> WinningNumbers { get; private set; }
+        public List<int> Numbers { get; private set; }
+        public Card(string input) 
+        { 
+            WinningNumbers = new List<int>();
+            Numbers = new List<int>();
+            ParseInput(input);
+        }
+
+        private void ParseInput(string input)
+        {
+            var cardArray = input.Split(':');
+            Id = int.Parse(MyFileReader.FindAnIntegerInAString(cardArray[0]));
+            var numbersArray = cardArray[1].Split("|");
+            var winningNumbers = numbersArray[0].Split(" ").Where(str => !string.IsNullOrWhiteSpace(str)).ToArray();
+            for(int i = 0; i < winningNumbers.Length; i++)
+            {
+                var winningNumber = winningNumbers[i];
+                WinningNumbers.Add(int.Parse(MyFileReader.FindAnIntegerInAString(winningNumber)));
+            }
+
+            var playableNumbers = numbersArray[1].Split(" ").Where(str => !string.IsNullOrWhiteSpace(str)).ToArray();
+            for (int i = 0; i < playableNumbers.Length; i++)
+            {
+                var playableNumber = playableNumbers[i];
+                Numbers.Add(int.Parse(MyFileReader.FindAnIntegerInAString(playableNumber)));
+            }
+        }
+    }
+}
