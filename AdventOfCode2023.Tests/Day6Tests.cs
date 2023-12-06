0ef5ad745bcee67b3d8b25891595a4b4ffa17cc5
Pommier Valentin <pommier.valentin-ext@solvay.com>
2023-12-06 18:55:24 +0100

    feat: day 6 solve equation to find all possible holding values

---
+++ AdventOfCode2023.Tests/Day6Tests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCode2023Day6;
+
+namespace AdventOfCode2023.Tests
+{
+    public class Day6Tests
+    {
+        [Test]
+        public void Race_should_return_the_best_possible_hold_time()
+        {
+            var race = new Race(59);
+
+            (double, double) bestAvailableRange = race.FindHoldingTimeFromDistance(430);
+
+            Assert.That(bestAvailableRange, Is.EqualTo((50, 9)));
+        }
+
+        [Test]
+        public void Race_should_return_the_correct_number_of_possible_holding_value_to_beat_a_given_score()
+        {
+            var race = new Race(7);
+
+            (double, double) bestAvailableRange = race.FindHoldingTimeFromDistance(9);
+            double numbersOfHoldingValues = (bestAvailableRange.Item2 - bestAvailableRange.Item1) + 1;
+
+            Assert.That(numbersOfHoldingValues,Is.EqualTo(4d));
+        }
+    }
+}
---
+++ AdventOfCode2023Day6/Race.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023Day6
+{
+    public class Race
+    {
+        public int Time { get; set; }
+        public Race(int time) 
+        {
+            Time = time;
+        }
+
+        public (double, double) FindHoldingTimeFromDistance(int traveledDistance)
+        {
+            // it's a standard quadratic equation a²+bx+c=0
+            // we have to calculate discriminant represented by formula Δ = b2 - 4ac where b = Race time and c = distance traveled
+            double discriminant = Time * Time - 4 * 1 * traveledDistance;
+            // this is the two solutions given by Quadratic Equation
+            double firstBoundary = Math.Ceiling(Time - Math.Sqrt(discriminant)) / 2;
+            double secondBoundary = Math.Floor(Time + Math.Sqrt(discriminant)) / 2;
+
+            // Any value >= firstBoundary and any value <= secondBoundary will give a better traveledDistance for the race time
+            return (firstBoundary, secondBoundary);
+        }
+    }
+}
