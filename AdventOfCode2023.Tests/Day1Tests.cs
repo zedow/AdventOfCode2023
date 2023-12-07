61a52c8d928b386c3a048e349057c4ec27320486
Pommier Valentin <pommier.valentin-ext@solvay.com>
2023-12-07 11:41:34 +0100

    feat: day 1 parse calibration from string input

---
+++ AdventOfCode2023.Tests/Day1Tests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCode2023Day1;
+
+namespace AdventOfCode2023.Tests
+{
+    public class Day1Tests
+    {
+        [Test]
+        public void Calibration_should_return_first_and_last_digit_when_input_contains_at_least_two_digits()
+        {
+            var input = "1abc2";
+
+            var calibration = new Calibration();
+            int result = calibration.Calibrate(input);
+
+            Assert.That(result,Is.EqualTo(12));
+        }
+    }
+}
---
+++ AdventOfCode2023Day1/Calibration.cs
@@ -0,0 +1,18 @@
+﻿using Kernel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023Day1
+{
+    public class Calibration
+    {
+        public int Calibrate(string input)
+        {
+            List<int> digits = MyFileReader.ParseIntegersFromStringInputUsingRegex(input);
+            return int.Parse(digits.First().ToString() + digits.Last().ToString());
+        }
+    }
+}
