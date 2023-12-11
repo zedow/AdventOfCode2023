871390ce7c0bcabe8286dbe7d7981fed606c536a
Pommier Valentin <pommier.valentin-ext@solvay.com>
2023-12-11 19:38:49 +0100

    feat: day 11 setup

---
+++ AdventOfCode2023.Tests/Day11Tests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023.Tests
+{
+    public class Day11Tests
+    {
+
+    }
+}
