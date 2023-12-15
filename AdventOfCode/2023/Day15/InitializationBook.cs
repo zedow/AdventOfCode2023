28f17e389e31b88bdbb53102ca28562b175ac6dd
Pommier Valentin <pommier.valentin-ext@solvay.com>
2023-12-15 14:46:13 +0100

    feat: day 15 part one

---
+++ AdventOfCode/2023/Day15/InitializationBook.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCode.Kernel;
+using Kernel;
+
+namespace AdventOfCode._2023.Day15
+{
+    [Challenge("Lens Library", "2023/Day15/input.txt")]
+    public class InitializationBook : IChallenge
+    {
+        public object SolvePartOne(string input)
+        {
+            return input.Split(",").Sum(Hash);
+        }
+
+        public object SolvePartTwo(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Hash(string input)
+        {
+            return (
+                from chr in Enumerable.Range(0, input.Length)
+                select (int)input[chr]
+            ).Aggregate(0,(hashValue, next) => hashValue = ((hashValue + next) * 17) % 256);
+        }
+    }
+}
---
+++ AdventOfCode2023.Tests/2023/Day15/Day15Tests.cs
@@ -0,0 +1,32 @@
+﻿using AdventOfCode._2023.Day15;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023.Tests._2023.Day15
+{
+    public class Day15Tests
+    {
+        [Test]
+        public void Hash_should_return_a_number_below_256()
+        {
+            string input = "HASH";
+
+            var book = new InitializationBook();
+
+            Assert.That(book.Hash(input), Is.EqualTo(52));
+        }
+
+        [Test]
+        public void Solve_part_one_should_return_hash_sequence_and_return_total()
+        {
+            var input = File.ReadAllText("../../../2023/Day15/inputTest.txt");
+
+            var book = new InitializationBook();
+
+            Assert.That((int)book.SolvePartOne(input), Is.EqualTo(1320));
+        }
+    }
+}
