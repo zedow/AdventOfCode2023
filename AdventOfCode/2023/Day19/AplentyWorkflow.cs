60fba650f7caf523b12aad04177c094ac4ba1945
Pommier Valentin <pommier.valentin-ext@solvay.com>
2023-12-20 11:30:14 +0100

    feat: Day 19 part one

---
+++ AdventOfCode/2023/Day19/AplentyWorkflow.cs
@@ -0,0 +1,112 @@
+﻿using AdventOfCode.Kernel;
+using Kernel;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode._2023.Day19;
+
+
+using Rules = List<Operation>;
+using WorkFlow = Dictionary<string, List<Operation>>;
+public record Operation(char Category, string Destination, int Value, char Operator);
+
+[Challenge("Aplenty", "2023/Day19/input.txt")]
+public class AplentyWorkflow : IChallenge
+{
+    public static string PartDefinition = "xmas";
+    public object SolvePartOne(string input)
+    {
+        var rulesAndParts = input.Split("\r\n\r\n", StringSplitOptions.RemoveEmptyEntries);
+        WorkFlow workflow = ParseWorkFlows(rulesAndParts[0]);
+        List<int[]> parts = ParseParts(rulesAndParts[1]);
+        return parts.Select(part => ProcessPartThroughWorkflow(part, workflow)).Sum();
+    }
+
+    public object SolvePartTwo(string input)
+    {
+        return 0;
+    }
+
+    public int ProcessPartThroughWorkflow(int[] part, WorkFlow workFlow)
+    {
+        var nextRule = "in";
+        while(nextRule != "A" && nextRule != "R")
+        {
+            for(int i = 0; i < workFlow[nextRule].Count; i++)
+            {
+                var category = workFlow[nextRule].ElementAt(i).Category;
+                Operation operation = workFlow[nextRule].ElementAt(i);
+                var valueToMatch = category == 'r' ? 0 : part[PartDefinition.IndexOf(category)];
+                var nextDestination = Operate(operation, valueToMatch);
+                if (nextDestination != string.Empty)
+                {
+                    nextRule = nextDestination;
+                    break;
+                }
+            }
+        }
+
+        return nextRule == "A" ? part.Sum() : 0;
+    }
+
+    public string Operate(Operation operation, int Value)
+    {
+        if (operation.Operator == '>')
+            return Value > operation.Value ? operation.Destination : string.Empty;
+        else if (operation.Operator == '<')
+            return Value < operation.Value ? operation.Destination : string.Empty;
+        else
+            return operation.Destination;
+    }
+
+    public List<int[]> ParseParts(string input)
+    {
+        var lines = input.Split("\r\n");
+        return (
+            from irow in Enumerable.Range(0, lines.Length)
+            let numbers = MyFileReader.ParseIntegersFromStringInputUsingRegex(lines[irow])
+            select new int[4] { numbers[0], numbers[1], numbers[2], numbers[3] }
+        ).ToList();
+    }
+
+    public WorkFlow ParseWorkFlows(string input)
+    {
+        var lines = input.Split("\r\n");
+        var workFlows = new WorkFlow();
+        foreach(var line in lines)
+        {
+            var workflowName = line.Substring(0, line.IndexOf('{'));
+            var stringRules = line.Substring(line.IndexOf('{') + 1, line.Length - (line.IndexOf('{') + 2)).Split(",");
+            var rules = new Rules();
+            foreach(var rule in stringRules)
+            {
+                var operation = BuildRule(rule);
+                rules.Add(operation);
+            }
+            workFlows.Add(workflowName,rules);
+        }
+        return workFlows;
+    }
+
+    // r doesn't exist, it's a random char because this rule doesn't fit any part category
+    public Operation BuildRule(string rule)
+    {
+        var operationAndDestination = rule.Split(":");
+        int valueComparison = MyFileReader.ParseIntegersFromStringInputUsingRegex(operationAndDestination[0]).FirstOrDefault();
+        if (operationAndDestination.Length == 1)
+            return new Operation('r', operationAndDestination[0], 0,'@');
+
+        string destination = operationAndDestination[1];
+        char category = operationAndDestination[0][0];
+        char operationSymbol = operationAndDestination[0][1];
+        return new Operation(category, destination, valueComparison, operationSymbol);
+    }
+}
+
---
+++ AdventOfCode2023.Tests/2023/Day19/Day19Tests.cs
@@ -0,0 +1,63 @@
+﻿using AdventOfCode._2023.Day19;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023.Tests._2023.Day19
+{
+    public class Day19Tests
+    {
+        [Test]
+        public void BuildRule_should_return_Operation_when_a_string_is_provided()
+        {
+            var rule = "a<2006:qkq";
+
+            var aplenty = new AplentyWorkflow();
+            var operation = aplenty.BuildRule(rule);
+
+            Assert.IsNotNull(operation);
+            Assert.That(operation.Category,Is.EqualTo('a'));
+            Assert.That(aplenty.Operate(operation,1), Is.EqualTo("qkq"), "because 1 < 2006");
+        }
+
+        [Test]
+        public void ParseWorkFlows_should_return_all_workflows()
+        {
+            var stringWorkflows = "px{a<2006:qkq,m>2090:A,rfg}";
+
+            var aplenty = new AplentyWorkflow();
+            var workflows = aplenty.ParseWorkFlows(stringWorkflows);
+
+            Assert.IsNotNull(workflows);
+            Assert.That(workflows.First().Key, Is.EqualTo("px"));
+            Assert.That(aplenty.Operate(workflows.First().Value.First(),1), Is.EqualTo("qkq"), "because 1 < 2006");
+        }
+
+        [Test]
+        public void ParseParts_should_return_parts_from_string()
+        {
+            var stringParts = "{x=787,m=2655,a=1222,s=2876}\r\n{x=1679,m=44,a=2067,s=496}\r\n{x=2036,m=264,a=79,s=2244}\r\n{x=2461,m=1339,a=466,s=291}\r\n{x=2127,m=1623,a=2188,s=1013}";
+
+            var aplenty = new AplentyWorkflow();
+            var parts = aplenty.ParseParts(stringParts);
+
+            Assert.That(parts.Count(), Is.EqualTo(5));
+            Assert.That(parts.First()[3], Is.EqualTo(2876));
+        }
+
+        [Test]
+        public void ProcessPartThroughWorkflow()
+        {
+            var stringWorkflows = "in{a<2006:qkq,m>2090:A,rfg}";
+            var aplenty = new AplentyWorkflow();
+            var workflow = aplenty.ParseWorkFlows(stringWorkflows);
+            var part = new int[4] { 0, 2091, 2007, 0 };
+
+            var result = aplenty.ProcessPartThroughWorkflow(part, workflow);
+
+            Assert.That(result,Is.EqualTo(2091 + 2007));
+        }
+    }
+}
