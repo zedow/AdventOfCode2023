faa5ea9ccb83c2646b89bd2aeee88903e9556602
Pommier Valentin <pommier.valentin-ext@solvay.com>
2024-01-16 14:32:54 +0100

    feat: 2022 day 1

---
+++ AdventOfCode/2022/Day01/CalorieCounting.cs
@@ -0,0 +1,23 @@
+﻿using AdventOfCode.Kernel;
+using Kernel;
+
+namespace AdventOfCode._2022.Day01;
+
+[Challenge("Calorie Counting","2022/Day01/input.txt")]
+public class CalorieCounting : IChallenge
+{
+    public object SolvePartOne(string input) => ParseInput(input).Select(calories => calories.Sum()).Max();
+
+    public object SolvePartTwo(string input)
+        => ParseInput(input).Select(calories => calories.Sum()).OrderDescending().Take(3).Sum();
+
+    public List<int[]> ParseInput(string input)
+    {
+        var blocks = input.Split("\r\n\r\n",StringSplitOptions.RemoveEmptyEntries);
+        return (
+            from iblock in Enumerable.Range(0,blocks.Count())
+            select MyFileReader.ParseIntegers(blocks[iblock]).ToArray()
+        ).ToList();
+    } 
+}
+
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -9,7 +9,7 @@ using System.Reflection;
 var types = Assembly.GetExecutingAssembly().GetTypes().Where(t => t.IsDefined(typeof(ChallengeAttribute)));
 
 
-var challengeToRun = "Sand Slabs";
+var challengeToRun = "Calorie Counting";
 
 Type? type = Assembly.GetExecutingAssembly().GetTypes().FirstOrDefault(t => t.IsDefined(typeof(ChallengeAttribute)) 
     && t?.GetCustomAttribute<ChallengeAttribute>()?.GetName() == challengeToRun);
